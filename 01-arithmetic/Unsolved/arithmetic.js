d7b8420d9b97a5742a97570648ac3671b04c8e6c
Jason Bailey <jmbailey816@gmail.com>
2021-08-31 17:17:45 -0400

    99-over-the-road added, not solved yet

--- 01-arithmetic/Unsolved/arithmetic.js
+++ 01-arithmetic/Unsolved/arithmetic.js
@@ -1,17 +1,27 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {
-    return num1 + num2;
+var add = function (num1, num2) {
+  return num1 + num2;
 };
 
-var subtract = function(num1, num2) {
-    return num1 - num2;
+var subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-var multiply = function(num1, num2) {
-    return num1 * num2;
+var multiply = function (num1, num2) {
+  return num1 * num2;
 };
 
-var divide = function(num1, num2) {
-    return num1 / num2;
+var divide = function (num1, num2) {
+  return num1 / num2;
 };
+
+//
+// example from: https://madisonkanna.com/howtosolvecodingproblems/
+//
+const a = 3;
+const b = 5;
+function addition(a, b) {
+  return a + b;
+}
+console.log(a + b);
