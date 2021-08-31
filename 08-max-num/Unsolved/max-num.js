00e10904b78538acd8b7bb3613eb5bb4cc43fbac
Jason Bailey <jmbailey816@gmail.com>
2021-08-31 14:38:05 -0400

    08 max num added

---
+++ 08-max-num/Unsolved/max-num.js
@@ -0,0 +1,11 @@
+// Write code to return the largest number in the given array
+
+var maxNum = function(arr) {
+    // console.log(Math.max(...arr));
+    return Math.max(...arr);
+};
+
+// var arr = [2, 3, -2, 99, 100, 2222, 321];
+
+// console.log(Math.max(...arr));
+
