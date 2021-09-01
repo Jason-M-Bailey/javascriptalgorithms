c53a134fcc45031ec46c3294a8f3da5bbf1a6010
Jason Bailey <jmbailey816@gmail.com>
2021-08-31 20:56:54 -0400

    12-title-case solved

--- 12-title-case/Unsolved/title-case.js
+++ 12-title-case/Unsolved/title-case.js
@@ -1,3 +1,26 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function (str) {
+  var result = [];
+
+  // split the string based on spaces --> " "
+  var words = str.split(" ");
+
+  // for loop through each word in the array
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i].split("");
+
+    // toUpperCase the first letter [0] of the string
+    word[0] = word[0].toUpperCase();
+
+    // now the words are capitalized but are in an array rather than a string
+
+    // use .join to combine the word back together
+    result.push(word.join(""));
+  }
+
+  // combine the array back into a string
+  return result.join(" ");
+};
+
+// https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
