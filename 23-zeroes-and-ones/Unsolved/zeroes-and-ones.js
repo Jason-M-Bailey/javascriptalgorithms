d0d310ad0b35705df78b4a0ff0abc18ddedc9397
Jason Bailey <jmbailey816@gmail.com>
2021-09-06 09:39:59 -0400

    23-zeroes-and-ones solved

--- 23-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ 23-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -2,4 +2,23 @@
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+var zeroesAndOnes = function(str) {
+    var zeroCount = 0;
+    var oneCount = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === "0") {
+            zeroCount++;
+        } else {
+            oneCount++;
+        }
+    }
+
+    if (zeroCount === oneCount) {
+        return true;
+    } else {
+        return false;
+    }
+    
+};
+
